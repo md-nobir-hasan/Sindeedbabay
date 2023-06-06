d550e1f5a822e9217f31f9be8a94f2ba0fffce67
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-06-07 00:11:31 +0600

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MFPTFKX');</script>
+    <!-- End Google Tag Manager -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>মরিয়ম ফুল &#8211; মরিয়ম ফুল</title>
@@ -12,6 +19,9 @@
         class="home cartflows_step-template cartflows_step-template-cartflows-canvas page page-id-26 theme-astra woocommerce-checkout woocommerce-page woocommerce-no-js ast-desktop ast-page-builder-template ast-no-sidebar astra-4.1.5 ast-single-post ast-inherit-site-logo-transparent ast-hfb-header cartflows-1.11.10 elementor-default elementor-kit-6 elementor-page elementor-page-26 cartflows-canvas">
 
         @yield('page_content')
-
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFPTFKX"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
     </body>
 </html>
