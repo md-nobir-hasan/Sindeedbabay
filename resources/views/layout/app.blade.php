c7b08ba825ef983355d5cd849f695c15ff27a4a0
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-06-07 00:31:04 +0600

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,6 +3,24 @@
     <head>
         <meta name="facebook-domain-verification" content="n8stagltyo6drwnyc3p6adnmsl5c1i" />
 
+        <!-- Facebook Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '770621304525528');
+    fbq('track', 'PageView');
+  </script>
+  <noscript>
+    <img height="1" width="1" style="display:none"
+         src="https://www.facebook.com/tr?id=770621304525528&ev=PageView&noscript=1"/>
+  </noscript>
+  <!-- End Facebook Pixel Code -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>মরিয়ম ফুল &#8211; মরিয়ম ফুল</title>
@@ -20,23 +38,7 @@
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-MFPTFKX');</script>
     <!-- End Google Tag Manager -->
-    <!-- Facebook Pixel Code -->
-        <script>
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '770621304525528');
-            fbq('track', 'PageView');
-        </script>
-        <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=770621304525528&ev=PageView&noscript=1"
-        /></noscript>
-  <!-- End Facebook Pixel Code -->
+
     </head>
     <body
         class="home cartflows_step-template cartflows_step-template-cartflows-canvas page page-id-26 theme-astra woocommerce-checkout woocommerce-page woocommerce-no-js ast-desktop ast-page-builder-template ast-no-sidebar astra-4.1.5 ast-single-post ast-inherit-site-logo-transparent ast-hfb-header cartflows-1.11.10 elementor-default elementor-kit-6 elementor-page elementor-page-26 cartflows-canvas">
