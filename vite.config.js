502aa6fab00901d161cc2e7d66affcae5dcffbf8
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-06-06 20:36:31 +0600

    build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css','resources/css/thank.css','resources/css/bootstrap53.min.css','resources/js/jquery37.min.js', 'resources/js/app.js','resources/js/frontend.js','resources/js/bootstrap53.min.js'],
+            input: ['resources/css/app.css','resources/css/thank.css','resources/css/bootstrap53.min.css', 'resources/js/app.js','resources/js/frontend.js','resources/js/bootstrap53.min.js'],
             refresh: true,
         }),
     ],
